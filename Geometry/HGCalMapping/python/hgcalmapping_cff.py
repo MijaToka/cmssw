736d1a5902b6972a7c2059408d173e437fcdba9f
Mijail Tokarev <mijail.tokarev@ug.uchile.cl>
2025-07-02 14:36:45 +0200

    Change to the hgcalmapping_cff to allow running of just DAQ module locators.

--- Geometry/HGCalMapping/python/hgcalmapping_cff.py
+++ Geometry/HGCalMapping/python/hgcalmapping_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 def customise_hgcalmapper(process,
                           modules = 'Geometry/HGCalMapping/data/ModuleMaps/modulelocator_test.txt',
                           sicells = 'Geometry/HGCalMapping/data/CellMaps/WaferCellMapTraces.txt',
-                          sipmcells = 'Geometry/HGCalMapping/data/CellMaps/channels_sipmontile.hgcal.txt'):
+                          sipmcells = 'Geometry/HGCalMapping/data/CellMaps/channels_sipmontile.hgcal.txt',
+                          trigmodule = 'Geometry/HGCalMapping/data/ModuleMaps/modulelocator_trigger_test.txt'):
     """the following function configures the mapping producers
     NOTE: for production-targetted configs should be avoided as it checks if the process as 
     already the Accelerators sequence loaded, if not it loads it to the process"""
@@ -14,7 +15,7 @@ def customise_hgcalmapper(process,
     process.hgCalMappingESProducer.sipm = cms.FileInPath(sipmcells)
 
     process.load('Geometry.HGCalMapping.hgCalMappingTriggerESProducer_cfi')
-    process.hgCalMappingTriggerESProducer.modules = cms.FileInPath(modules)
+    process.hgCalMappingTriggerESProducer.modules = cms.FileInPath(trigmodule)
     process.hgCalMappingTriggerESProducer.si = cms.FileInPath(sicells)
     process.hgCalMappingTriggerESProducer.sipm = cms.FileInPath(sipmcells)
 
@@ -27,11 +28,12 @@ def customise_hgcalmapper(process,
     process.hgCalMappingModuleESProducer = cms.ESProducer('hgcal::HGCalMappingModuleESProducer@alpaka',
                                                           filename=cms.FileInPath(modules),
                                                           moduleindexer=cms.ESInputTag(''))
-    process.hgCalMappingModuleTriggerESProducer = cms.ESProducer('hgcal::HGCalMappingTriggerModuleESProducer@alpaka',
-                                                                filename=cms.FileInPath(modules),
-                                                                moduleindexer=cms.ESInputTag(''))
     process.hgCalDenseIndexInfoESProducer = cms.ESProducer('hgcal::HGCalDenseIndexInfoESProducer@alpaka',
                                                            moduleindexer=cms.ESInputTag('') )
+
+    process.hgCalMappingModuleTriggerESProducer = cms.ESProducer('hgcal::HGCalMappingTriggerModuleESProducer@alpaka',
+                                                                filename=cms.FileInPath(trigmodule),
+                                                                moduleindexer=cms.ESInputTag(''))
     process.hgCalDenseIndexTriggerInfoESProducer = cms.ESProducer('hgcal::HGCalDenseIndexTriggerInfoESProducer@alpaka',
                                                            moduleindexer=cms.ESInputTag('') )
 
